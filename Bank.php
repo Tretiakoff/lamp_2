d89e2163056cacf67a62b670489dd865853f0826
Tretiakoff <c.tretiakoff@gmail.com>
2016-02-02 22:38:28 +0100

    ex1

---
+++ Bank.php
@@ -0,0 +1,7 @@
+<?php
+require_once ("Player.php");
+class Bank extends Player{
+    public function __construct(){
+        parent::__construct("Banque");
+    }
+}
---
+++ Player.php
@@ -0,0 +1,24 @@
+<?php
+require_once ("Card.php");
+class Player{
+    protected $hand; //sert à stocker les cartes ue le joueur possède
+    protected $pseudo;
+    public function __construct($toto){
+        $this->hand = [];
+        $this->pseudo = $toto;
+    }
+    public function take($cards){ //$cards = [Card,Card]
+        //TODO : récupère les cartes passées en paramètre
+        //et les place dans $this->hand'
+        foreach($cards as $card){
+            $this->hand[] = $card;
+        }
+    }
+    public function getHandValue(){
+        $panier = 0;
+        foreach($this->hand as $card){
+            $panier += $card->getValue();
+        }
+        return $panier;
+    }
+}
